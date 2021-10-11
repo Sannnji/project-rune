bf6d985fef9a0d81787585947b968e1a621b599e
Sannnji <jamesji.penfield@protonmail.com>
2021-10-11 19:29:18 -0400

    changed structure of returning data

--- backend/api/runes.controller.js
+++ backend/api/runes.controller.js
@@ -4,7 +4,15 @@ export default class RunesController {
   static async apiGetRunes(req, res, next) {
     try {
       const response = await RunesDAO.getRunes();
-      res.json(response);
+
+      const runes = {
+        low_rune: response[0].low_rune,
+        low_mid_rune: response[1].low_mid_rune,
+        mid_rune: response[2].mid_rune,
+        high_rune: response[3].high_rune,
+      };
+
+      res.json(runes);
     } catch (err) {
       console.log(`api, ${err}`);
       res.status(500).json({ error: err });
--- backend/api/runewords.controller.js
+++ backend/api/runewords.controller.js
@@ -4,7 +4,14 @@ export default class RunewordsController {
   static async apiGetRunewords(req, res, next) {
     try {
       const response = await RunewordsDAO.getRunewords();
-      res.json(response);
+      const runes = {
+        chest_armor: response[0].chest_armor,
+        head_armor: response[1].head_armor,
+        shield: response[2].shield,
+        weapon: response[3].weapon,
+      };
+
+      res.json(runes);
     } catch (err) {
       console.log(`api, ${err}`);
       res.status(500).json({ error: err });
