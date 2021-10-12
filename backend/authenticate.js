333b4824a91ae0839472e23c91e39f9ac1a6f401
Sannnji <jamesji.penfield@protonmail.com>
2021-10-12 19:58:22 -0400

    moved secret key

--- backend/authenticate.js
+++ backend/authenticate.js
@@ -1,15 +1,10 @@
-import { response } from "express";
 import jwt from "jsonwebtoken";
-
-export const getToken = (user) => {
-  return jwt.sign(user, "SECRET_KEY");
-};
 
 export const verifyUser = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  jwt.verify(token, "SECRET_KEY", (err, user) => {
+  jwt.verify(token, process.env.SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
--- backend/dao/usersDAO.js
+++ backend/dao/usersDAO.js
@@ -77,8 +77,8 @@ export default class UsersDAO {
     try {
       const user = await users.findOne({ username: username });
       if (user.username === username && user.password === password) {
-        const token = jwt.sign(user, "SECRET_KEY", { expiresIn: 1000 });
-        
+        const token = jwt.sign(user, process.env.SECRET);
+
         return { token };
       }
     } catch (err) {
