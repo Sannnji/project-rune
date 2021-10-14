3e33b32f0b5d0f13d8528724fa3c7578a236d36b
Sannnji <jamesji.penfield@protonmail.com>
2021-10-14 14:18:50 -0400

    added card for 0 items in craftable array

--- frontend/src/components/inventory/Craftable.jsx
+++ frontend/src/components/inventory/Craftable.jsx
@@ -9,48 +9,55 @@ import {
 } from "@chakra-ui/react";
 
 export default function Craftable({ craftable, runewords }) {
-  return (
-    <Accordion allowToggle>
-      {craftable.map((runeword, index) => {
-        return (
-          <>
-            <AccordionItem key={index} border="none" bg="#090909" m={4}>
-              <AccordionButton
-                _focus={{ outline: "none", boxShadow: "none" }}
-                position="relative"
-              >
-                <Box flex="1" textAlign="center" color="#C7B377">
+  if (craftable.length === 0) {
+    return (
+      <Box align="center" bg="#090909" m={4} p={2} color="#C7B377">
+        You Don't Have Any Craftable Runewords
+      </Box>
+    );
+  } else
+    return (
+      <Accordion allowToggle>
+        {craftable.map((runeword, index) => {
+          return (
+            <>
+              <AccordionItem key={index} border="none" bg="#090909" m={4}>
+                <AccordionButton
+                  justifyContent="center"
+                  color="#C7B377"
+                  position="relative"
+                  _focus={{ outline: "none", boxShadow: "none" }}
+                >
                   {runeword}
-                </Box>
-                <AccordionIcon position="absolute" right={4} />
-              </AccordionButton>
+                  <AccordionIcon position="absolute" right={4} />
+                </AccordionButton>
 
-              <AccordionPanel textAlign="center" mt={-2}>
-                {Object.getOwnPropertyNames(runewords).map(
-                  // eslint-disable-next-line array-callback-return
-                  (property) => {
-                    const runeword2 = runewords[property].find(
-                      (rw) => rw.name === runeword
-                    );
-
-                    if (runeword2)
-                      return (
-                        <>
-                          <Text color="#797979">{runeword2.gear}</Text>
-                          <Text color="#C7B377">'{runeword2.recipe}'</Text>
-                          <Text>Level Requirement: {runeword2.lvl_req}</Text>
-                          {runeword2.stats.map((stat) => {
-                            return <Text color="#6969FF">{stat}</Text>;
-                          })}
-                        </>
+                <AccordionPanel textAlign="center" mt={-2}>
+                  {Object.getOwnPropertyNames(runewords).map(
+                    // eslint-disable-next-line array-callback-return
+                    (property) => {
+                      const runeword2 = runewords[property].find(
+                        (rw) => rw.name === runeword
                       );
-                  }
-                )}
-              </AccordionPanel>
-            </AccordionItem>
-          </>
-        );
-      })}
-    </Accordion>
-  );
+
+                      if (runeword2)
+                        return (
+                          <>
+                            <Text color="#797979">{runeword2.gear}</Text>
+                            <Text color="#C7B377">'{runeword2.recipe}'</Text>
+                            <Text>Level Requirement: {runeword2.lvl_req}</Text>
+                            {runeword2.stats.map((stat) => {
+                              return <Text color="#6969FF">{stat}</Text>;
+                            })}
+                          </>
+                        );
+                    }
+                  )}
+                </AccordionPanel>
+              </AccordionItem>
+            </>
+          );
+        })}
+      </Accordion>
+    );
 }
